a0e0406da70b7bbc0e04122ff94b5ea42074b149
hyun <hyun231366@gmail.com>
2023-07-03 18:23:42 +0900

    23.07.03|hj|profile거의 다 됨

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import './Profile.css'
+import './Profile.scss'
 
 const Profile = () => {
   
   return (
-    <div>
-      회원 정보 수정<br /> 
-      ID : <input type='text' name='id' placeholder='고정값' readOnly/> <br />      
-      PW : <input type='password' name='pw' placeholder='비밀번호'/> <br />      
-      Nick : <input type='text' name='nick' placeholder='닉네임'/> <br />      
-      Email : <input type='text' name='email' placeholder='이메일' />      
-    </div>
+    <>
+      회원 정보 수정  <br />
+        <div className='prof-main'>
+          <div className='profil'>
+            ID : <input type='text' name='id' placeholder='고정값' readOnly/> <br />      
+            PW : <input type='password' name='pw' placeholder='비밀번호'/>  <br />      
+            Nick : <input type='text' name='nick' placeholder='닉네임'/>  <br />      
+            Email : <input type='text' name='email' placeholder='이메일' />
+          </div>
+          <div className='image-profil'>
+            <img src={require("../img/profilImage.png")} />
+          </div>
+        </div>
+    </>
   )
 }
 
