4c1f3380a22736e750b46d65beabd91fc2f67a81
dongjun <sdj799@naver.com>
2023-07-03 19:14:35 +0900

    23.07.03 || dj || 로그인 구현중

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Intro from './layout/Intro';
+import { Route, Routes } from 'react-router';
+import Login from './user/Login';
+import Join from './user/Join';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login/>
+      {/* <div className='content-wrapper'>
+          <Routes>
+            <Route path='/' element={ <Intro/>}/>
+            <Route path='/login' element={ <Login/>}/>
+            <Route path='/join' element={ <Join/>}/>
+          </Routes>
+        </div> */}
     </div>
   );
 }
---
+++ src/layout/Intro.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './intro.scss';
+import { Link } from '@mui/material';
+
+const Intro = () => {
+
+  const textChange = e => {
+    e.target.textContent = '떠나볼까?';
+  }
+  const returnText = e => {
+    e.target.textContent = 'Tramovel'
+  }
+  return (
+    <>
+    <div className='intro-container'>
+
+        <Link to= '/Main' className='intro-title' onMouseEnter={textChange} onMouseLeave={returnText}>Tramovel</Link>
+
+        <span className='intro-name'></span>
+    </div>
+    </>
+  )
+};
+
+export default Intro;
