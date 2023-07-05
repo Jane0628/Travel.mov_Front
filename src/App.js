c99492f1c03cb4c62a4867ea2cedca13322f1816
Jane <ribbon0628@naver.com>
2023-07-05 14:21:18 +0900

    23.07.05 | jh | 메인 페이지 제작 시작

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { Route, Routes } from 'react-router-dom';
 import Profile from './component/user/Profile';
 import Intro from './component/layout/Intro';
 import Footer from './component/layout/Footer';
+import Main from './component/layout/Main';
 
 function App() {
   return (
     <>
       <Header />
-      <Profile />
-      <Footer />
+      <Main />
     </>
   );
 }
