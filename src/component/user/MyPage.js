47176bc57ef6311ed8de8a055ae4c1b91da43283
hyun <hyun231366@gmail.com>
2023-07-05 16:28:28 +0900

    23.07.05|hj|머지

--- src/component/user/MyPage.js
+++ src/component/user/MyPage.js
@@ -8,13 +8,18 @@ const MyPage = () => {
       <div className='my-page'>
           <div className='welcome'>
             <img src={require("../../img/profileImage.png")} />
-            심청이님 환영합니다.
+            <div className='nick'>심청이님</div>
+            <div className='nick-welcome'>환영합니다</div>
+            <div className='photo'>프로필 사진 바꾸기 {'>'}</div>
           </div>
           <div className='page-menu'>
             <span>회원 정보 수정하기</span>
-            <span>작성한 여행 후기</span>
-            <span>뭘 추가 해야할까</span>
-            <span>하나 더 추가 하고싶다</span>
+            <span>영화 촬영지 여행 후기</span>
+            <span>찜 목록</span>
+            <span>으핫</span>
+            <div className='delete'>
+              <a>회원 탈퇴</a>
+            </div>
           </div>
       </div>
     </>
