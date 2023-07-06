b64985f7e8407f6c56b5cc00bc00370741732bed
hyun <hyun231366@gmail.com>
2023-07-06 11:30:15 +0900

    23.07.06|hj|프로필 mui 수정완료?

--- src/component/user/MyPage.js
+++ src/component/user/MyPage.js
@@ -1,25 +1,49 @@
 import React from 'react'
 import "../../design/mypage.scss"
+import { Button, Grid,} from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const MyPage = () => {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#7b8ce0',
+      },
+    },
+  });
+
   return (
     <>
       <h1>My Page</h1>
       <div className='my-page'>
           <div className='welcome'>
-            <img src={require("../../img/profileImage.png")} />
-            <div className='nick'>심청이님</div>
-            <div className='nick-welcome'>환영합니다</div>
-            <div className='photo'>프로필 사진 바꾸기 {'>'}</div>
+            <Grid item xs={8}>                            
+              <img                                    
+                src={require("../../img/profileImage.png") }
+                alt="profile"                                    
+                />
+            </Grid>
+            <div className='nick'>
+              심청이님 <br />
+              환영합니다
+            </div>
           </div>
           <div className='page-menu'>
+
             <span>회원 정보 수정하기</span>
             <span>영화 촬영지 여행 후기</span>
             <span>찜 목록</span>
             <span>으핫</span>
+
             <div className='delete'>
-              <a>회원 탈퇴</a>
-            </div>
+              <ThemeProvider theme={theme}>
+                <Button variant="contained" color="primary" className={`custom-button small`}>
+                  회원 탈퇴
+                </Button>
+              </ThemeProvider>
+              </div>
+
           </div>
       </div>
     </>
