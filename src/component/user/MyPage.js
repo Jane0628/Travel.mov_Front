08d0ddb77592bbe0d7c37bf188c8f0e405de969f
hyun <hyun231366@gmail.com>
2023-07-06 15:07:58 +0900

    23.07.06|hj|Profile 무이 바꾸고 MyPage 하는중

--- src/component/user/MyPage.js
+++ src/component/user/MyPage.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import "../../design/mypage.scss"
-import { Button, Grid,} from '@mui/material'
+import { Button, Grid, Container} from '@mui/material'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 
 const MyPage = () => {
 
@@ -15,37 +16,39 @@ const MyPage = () => {
 
   return (
     <>
-      <h1>My Page</h1>
-      <div className='my-page'>
-          <div className='welcome'>
-            <Grid item xs={8}>                            
-              <img                                    
-                src={require("../../img/profileImage.png") }
-                alt="profile"                                    
-                />
-            </Grid>
-            <div className='nick'>
-              심청이님 <br />
-              환영합니다
-            </div>
-          </div>
-          <div className='page-menu'>
-
-            <span>회원 정보 수정하기</span>
-            <span>영화 촬영지 여행 후기</span>
-            <span>찜 목록</span>
-            <span>으핫</span>
-
-            <div className='delete'>
-              <ThemeProvider theme={theme}>
-                <Button variant="contained" color="primary" className={`custom-button small`}>
-                  회원 탈퇴
-                </Button>
-              </ThemeProvider>
+      <Container>
+        <h1>My Page</h1>
+        <div className='my-page'>
+            <div className='welcome'>
+              <Grid item xs={8}>                            
+                <img                                    
+                  src={require("../../img/profileImage.png") }
+                  alt="profile"                                    
+                  />
+              </Grid>
+              <div className='nick'>
+                심청이님 <br />
+                환영합니다
               </div>
+            </div>
+            <div className='page-menu'>
 
-          </div>
-      </div>
+              <Link to="/profile" className='link'>프로필 수정</Link>
+              <span>영화 촬영지 여행 후기</span>
+              <span>찜 목록</span>
+              <span>으핫</span>
+
+              <div className='delete'>
+                <ThemeProvider theme={theme}>
+                  <Button variant="contained" color="primary" className={`custom-button small`}>
+                    회원 탈퇴
+                  </Button>
+                </ThemeProvider>
+                </div>
+
+            </div>
+        </div>
+      </Container>
     </>
   )
 }
--- src/component/user/Profile.js
+++ src/component/user/Profile.js
@@ -3,6 +3,7 @@ import '../../design/profile.scss'
 import { Grid, Button, TextField } from '@mui/material';
 import { Input } from '@mui/base';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Container } from 'reactstrap';
 
 const Profile = () => {
 
@@ -16,84 +17,86 @@ const Profile = () => {
 
   return (
     <>
-      <h1>회원 정보 수정</h1>
-      <div className='prof-main'>
-        <div className='image'>
-          <Grid item xs={8}>                            
-            <img                                    
-              src={require("../../img/profileImage.png") }
-              alt="profile"                                    
-            />
-          </Grid>
-          <label className='image-change' htmlFor='profile-img'>프로필 이미지 설정</label>
-        </div>
-        <div className='profile'>
-          <div className="right">            
+      <Container>
+        <h1>프로필 수정</h1>
+        <div className='prof-main'>
+          <div className='image'>
             <Grid item xs={8}>
-              <TextField
-                type="text"
-                required
-                fullWidth
-                id="id"
-                label="아이디"
-                name="id"
-                value={'SimChung2'}
-                readOnly
+              <img
+                src={require("../../img/profileImage.png") }
+                alt="profile"
               />
-            </Grid>            
-            <Grid item xs={8}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="password"
-                label="비밀번호"
-                name="password"
-                autoComplete="password"
-                type="password"
-              />
-            </Grid>            
-            <Grid item xs={8}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="nick"
-                label="닉네임"
-                name="nick"
-                autoComplete="nick"
-              />
-            </Grid>            
-            <Grid item xs={8}>
-            <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="이메일"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>            
+            </Grid>
+            <label className='image-change' htmlFor='profile-img'>프로필 이미지 설정</label>
           </div>
-            <a>
-              <div className='change'>
-              <ThemeProvider theme={theme}>
-                <Button variant="contained" color="primary" className={`custom-button small`}>
-                  변경할래요
-                </Button>
-              </ThemeProvider>
-              </div>
-              <div className='no-change'>
-              <ThemeProvider theme={theme}>
-                <Button variant="contained" color="primary" className={`custom-button small`}>
-                  변경안해요
-                </Button>
-              </ThemeProvider>
-              </div>
-            </a>
+          <div className='profile'>
+            <div className="right">
+              <Grid item xs={8}>
+                <TextField
+                  type="text"
+                  required
+                  fullWidth
+                  id="id"
+                  label="아이디"
+                  name="id"
+                  value={'SimChung2'}
+                  readOnly
+                />
+              </Grid>            
+              <Grid item xs={8}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="password"
+                  label="비밀번호"
+                  name="password"
+                  autoComplete="password"
+                  type="password"
+                />
+              </Grid>            
+              <Grid item xs={8}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="nick"
+                  label="닉네임"
+                  name="nick"
+                  autoComplete="nick"
+                />
+              </Grid>            
+              <Grid item xs={8}>
+              <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="email"
+                  label="이메일"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>            
+            </div>
+              <a>
+                <div className='change'>
+                <ThemeProvider theme={theme}>
+                  <Button variant="contained" color="primary" className={`custom-button small`}>
+                    변경할래요
+                  </Button>
+                </ThemeProvider>
+                </div>
+                <div className='no-change'>
+                <ThemeProvider theme={theme}>
+                  <Button variant="contained" color="primary" className={`custom-button small`}>
+                    변경안해요
+                  </Button>
+                </ThemeProvider>
+                </div>
+              </a>
+          </div>
         </div>
-      </div>
+      </Container>
     </>
   )
 }
