3b839f4a97d0c88297044e8770a2e8c795de09af
kkang3617 <rkdgus3617@naver.com>
2023-07-10 16:23:55 +0900

    23.07.10 | kh | Sights.js 초안완성 기능은 아직없음.

--- src/component/layout/Sights.js
+++ src/component/layout/Sights.js
@@ -1,9 +1,7 @@
 import * as React from 'react'
 import { useNavigate } from 'react-router';
 import '../../design/sights.scss';
-import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -12,24 +10,20 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
+const cards = [1, 2, 3, 4, 5, 6];
+
 
 export default function Sights() {
 
   const redirection = useNavigate();
 
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <>
       <CssBaseline />
       <main>
         {/* Hero unit */}
@@ -114,6 +108,6 @@ export default function Sights() {
         </Typography>
       </Box>
       {/* End footer */}
-    </ThemeProvider>
+    </>
   );
 }
