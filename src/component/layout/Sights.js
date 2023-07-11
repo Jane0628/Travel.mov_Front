e570c8f10f3d2356dd7b52329fe87b740f027adb
kkang3617 <rkdgus3617@naver.com>
2023-07-11 18:29:16 +0900

    23.07.11 | kh | copyright 추가

--- src/component/layout/Sights.js
+++ src/component/layout/Sights.js
@@ -12,8 +12,20 @@ import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 
-
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://Tramovle.com/">
+        Tramovle.com
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 // const cards = [1, 2, 3, 4, 5, 6];
 const cards = [
@@ -129,23 +141,10 @@ export default function Sights() {
               </Grid>
             ))}
           </Grid>
+          <br/>
+        <Copyright />
         </Container>
       </main>
-      {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
-      </Box>
-      {/* End footer */}
     </>
   );
 }
