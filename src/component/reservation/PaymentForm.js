da7f802e3d0660470882274cd65f063b97a3175a
dongjun <sdj799@naver.com>
2023-07-13 18:39:40 +0900

    23.07.13 | dj | 카카오페이 멸망중

--- src/component/reservation/PaymentForm.js
+++ src/component/reservation/PaymentForm.js
@@ -1,18 +1,54 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function PaymentForm() {
+  const redirection = useNavigate();
+  const [state, setState] = React.useState({
+    next_redirect_pc_url: "",
+    tid: "",
+  });
+
+  const preparePayment = async () => {
+    const res = await fetch("https://kapi.kakao.com/v1/payment/ready", {
+      method: "POST",
+      headers: {
+        Authorization: "KakaoAK ccabfd3783b9f3b5c1fa775f38b008ef", // 발급받은 API 키
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+      },
+      body: JSON.stringify({
+        cid: "TC0ONETIME", // 가맹점의 CID
+        partner_order_id: "partner_order_id", // 가맹점에서 관리하는 주문번호
+        partner_user_id: "partner_user_id", // 가맹점에서 관리하는 회원고유번호
+        item_name: "초코파이",
+        quantity: 1,
+        total_amount: 2200, // 결제 금액
+        vat_amount: 200,
+        tax_free_amount: 0,
+        approval_url: "http://localhost:3000/", // 결제 성공시 리다이렉트할 URL
+        fail_url: "http://localhost:3000/", // 결제 취소시 리다이렉트할 URL
+        cancel_url: "http://localhost:3000/", // 결제 실패시 리다이렉트할 URL
+      }),
+    });
+    console.log(res);
+
+    const { next_redirect_pc_url, tid } = await res.json();
+    console.log(tid);
+    console.log(next_redirect_pc_url);
+  };
+
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-        Payment method
+        카카오페이로 결제하기
       </Typography>
       <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
+        {/* <Grid item xs={12} md={6}>
           <TextField
             required
             id="cardName"
@@ -52,7 +88,11 @@ export default function PaymentForm() {
             autoComplete="cc-csc"
             variant="standard"
           />
-        </Grid>
+        </Grid> */}
+        <img
+          src={require("../../img/payment_icon_yellow_large.png")}
+          onClick={preparePayment}
+        />
         {/* <Grid item xs={12}>
           <FormControlLabel
             control={<Checkbox color="secondary" name="saveCard" value="yes" />}
