2e6f0e8d801fd5cf07beeb5e1dd1d7aa1b79af18
kkang3617 <rkdgus3617@naver.com>
2023-07-14 16:10:05 +0900

    23.07.14 | kh | 머지전 커밋

---
+++ src/component/view/movie/TodaysMovieDetail.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NowPlayingMovie from "./pages/NowPlayingMovie";
+import UpcomingMovie from "./pages/UpcomingMovie";
+import Detail from "./pages/Detail";
+import MovieStateProvider from "./provider/MovieStateProvider";
+import "./common.css";
+import Navbar from "./component/Navbar";
+
+function TodaysMovieDetail() {
+  return (
+    <>
+      <BrowserRouter basename="/react-movie-app">
+        <MovieStateProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/now_playing" element={<NowPlayingMovie />} />
+            <Route path="/upcoming" element={<UpcomingMovie />} />
+            <Route path="/movie/:movie_id" element={<Detail />} />
+          </Routes>
+        </MovieStateProvider>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default TodaysMovieDetail;
