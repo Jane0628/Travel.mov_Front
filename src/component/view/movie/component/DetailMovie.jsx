0aa3c78871d48a75e894ff369bb65f1741e27d4c
hyun <hyun231366@gmail.com>
2023-08-03 12:40:02 +0900

    23.08.03|hj|Detail 수정완료

--- src/component/view/movie/component/DetailMovie.jsx
+++ src/component/view/movie/component/DetailMovie.jsx
@@ -68,7 +68,9 @@ const Description = styled.div`
         h1.title {
           word-break: keep-all;
         }
-
+        h2.title {
+          word-break: keep-all;
+        }
         .release-date{
             overflow: nowrap;
         }
@@ -104,10 +106,15 @@ const Description = styled.div`
         }
     }
 `;
+const MapContainer = styled.div`
+  margin-left: 100px;
+  margin-top: 100px;
+`;
 export default function DetailMovie({ movieInfo, imageUrl }) {
   const { poster_path, title, genres, imdb_id, release_date, vote_average, tagline, overview } = movieInfo;
   const [locations, setLocations] = useState([]);
   const [gooLocation, setGooLocation] = useState('');
+  const [showMap, setShowMap] = useState(false); // 지도 표시 여부 상태 추가
 
   useEffect(() => {
     getLocations();
@@ -142,29 +149,54 @@ export default function DetailMovie({ movieInfo, imageUrl }) {
     setGooLocation(e.target.textContent);
   }
 
+  const toggleMap = () => {
+    setShowMap(prevShowMap => !prevShowMap);
+  }
   return (
     <DetailMovieWrapper>
-      <div className="movie">
+      <h1 className="title" style={{fontSize: '55px'}}>{title}</h1>
+      <div className="movie" style={{position: 'relative'}}>
         <img className="rounded" src={imageUrl + poster_path} />
-        <Description>
-          <h1 className="title">{title}</h1>
-          <div className="genres">
-            {genres.map(genre =>
-              <div className="genre" key={genre.id}>{genre.name}</div>)}</div>
-          <div className="sub-info">
-            <div className="release-date">개봉일 : {release_date}</div>
-          </div>
-          <div className="vote-average">
-            <Star vote_average={vote_average} />
-            <div>{vote_average}</div>
-          </div>
-          <div className="overviewInfo">
-            <div className="tagline">{tagline ? `"${tagline}"` : ''}</div>
-            <div className="overview">{overview}</div>
-          </div>
-        </Description>
-      </div>
-      <GooMap location={gooLocation} />
+          {showMap ? (
+            <MapContainer>
+              <GooMap location={gooLocation} /> // showMap이 true인 경우 지도 표시
+            </MapContainer>
+          ) : (
+            <Description>
+            <h1 style={{fontSize: '30px', marginLeft: '0'}}>영화 정보</h1>
+            <div className="genres">
+              {genres.map(genre =>
+                <div className="genre" key={genre.id}>{genre.name}</div>)}
+            </div>
+            <div className="sub-info">
+              <div className="release-date">개봉일 : {release_date}</div>
+            </div>
+            <div className="vote-average">
+              <Star vote_average={vote_average} />
+              <div>{vote_average}</div>
+            </div>
+            <div className="overviewInfo">
+              <div className="tagline">{tagline ? `"${tagline}"` : ''}</div>
+              <div className="overview">{overview}</div>
+              {showMap && <GooMap location={gooLocation} />} {/* 지도 표시 여부에 따라 조건부 렌더링 */}
+            </div>
+            </Description>
+          )}
+          <Button 
+            onClick={toggleMap}
+            style={{
+              width: '180px',
+              height: '45px',
+              borderRadius: '30px',
+              background: '#7b8ce0',
+              color: '#ffffff',
+              position: 'absolute',
+              bottom: '0px',
+              right: '0px',
+              border: '2px solid #7b8ce0',
+              zIndex: 1000, // 다른 요소 위에 나타나도록 zIndex 설정
+            }}>지도 표시/숨기기</Button> {/* 버튼 클릭 시 지도 표시 여부를 토글 */}
+        </div>
       {
         locations.map((location, index) => (
           <Button key={index} onClick={searchLocation}>{location}</Button>
