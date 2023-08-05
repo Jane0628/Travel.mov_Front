9f2579e77be50f191cee8509c997d386329b19b7
dongjun <sdj799@naver.com>
2023-08-05 15:53:28 +0900

    23.08.05 | dj | 배포 설정 중

--- src/util/host-utils.js
+++ src/util/host-utils.js
@@ -1,12 +1,13 @@
 // 브라우저에서 현재 클라이언트의 호스트 이름 얻어오기
 const clientHostName = window.location.hostname;
+console.log(clientHostName);
 
 let backEndHostName; // 백엔드 서버 호스트 이름
 
 if (clientHostName === "localhost") {
   //개발 중
   backEndHostName = "http://localhost:8181";
-} else if (clientHostName === "http://tramovel.com") {
+} else if (clientHostName === "tramovel.com") {
   //배포해서 서비스 중
   backEndHostName = "http://13.209.179.148";
 }
